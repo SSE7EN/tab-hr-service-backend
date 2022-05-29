f12b0d5e1881d4a6fe6ef1f17b735438726eb9a0
Piotr Swierzy <piotr.swierzy@arianelabs.com>
2022-05-29 23:31:06 +0200

    Role fixed

--- src/main/java/pl/polsl/hrservice/user/controller/UserController.java
+++ src/main/java/pl/polsl/hrservice/user/controller/UserController.java
@@ -51,7 +51,7 @@ public class UserController {
     private final IUserPasswordService userPasswordService;
 
     @SecurityRequirement(name = "bearerAuth")
-    @PreAuthorize("hasAuthority(T(com.mynt.myntnear.user.enumerator.Role).ADMIN)")
+    @PreAuthorize("hasAuthority(T(pl.polsl.hrservice.user.enumerator).ADMIN)")
     @PostMapping("/admins")
     public UserDTO create(@RequestBody UserCreateDTO dto) {
         final var user = userCreateService.create(mapper.map(dto, Role.ADMIN));
